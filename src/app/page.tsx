e01f7431f8ffd3f04a8213885680212bfdb03486
Laura-Cintra <lauraocintra@gmail.com>
2024-10-25 12:10:04 -0300

    Ajustes no Header e Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import Cabebecalho from "@/components/Cabecalho";
 import Image from "next/image";
 import banner from "@/image/banner-home.png"
-import Rodape from "@/components/Rodape";
 
 export default function Home() {
   const  tailWind = "text-white font-bold flex items-center space-x-8 text-xl font-bebas links"
 
   return (
-    <div>
-      <Cabebecalho/>
-      <div className="bg-black flex justify-center">
-        <Image className= "max-w-[100%]" src={banner} alt="Imagem escrito marvel com fundo preto e os heróis da marvel compondo o fundo das letras"/>
-      </div>
-      <Rodape/>
+    <div className="bg-black flex justify-center">
+      <Image className= "max-w-[100%]" src={banner} alt="Imagem escrito marvel com fundo preto e os heróis da marvel compondo o fundo das letras"/>
     </div>
   );
 }
